a9c60d2036d8f4cd9c07930fa4b6aaab27701ec4
User <User@LAB3-10>
2021-11-24 14:33:52 +0200

    Signform

--- CheckItApp/CheckItApp/Models/Form.cs
+++ CheckItApp/CheckItApp/Models/Form.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 
 namespace CheckItApp.Models
 {
-    public partial class Form
+  
+    public class Form
     {
         public Form()
         {
             FormsOfGroups = new List<FormsOfGroup>();
+            Signforms = new List<Signform>();
         }
 
+       
         public string FormType { get; set; }
      
         public string Topic { get; set; }
@@ -19,15 +22,15 @@ namespace CheckItApp.Models
         public string MassageBody { get; set; }
         public int StatusOfTheMessage { get; set; }
         public int Sender { get; set; }
-      
-        public int FormId { get; set; }
      
+        public int FormId { get; set; }
+      
         public DateTime TimeSend { get; set; }
 
-      
         public virtual StaffMember SenderNavigation { get; set; }
-      
-        public virtual Signform Signform { get; set; }
+       
         public virtual List<FormsOfGroup> FormsOfGroups { get; set; }
+
+        public virtual List<Signform> Signforms { get; set; }
     }
 }
--- CheckItApp/CheckItApp/Models/Signform.cs
+++ CheckItApp/CheckItApp/Models/Signform.cs
@@ -5,17 +5,17 @@ using System.Collections.Generic;
 
 namespace CheckItApp.Models
 {
-   
-    public partial class Signform
+  
+    public class Signform
     {
-       
+        public int SignFormId { get; set; }
         public int IdOfForm { get; set; }
         public int PerentSignId { get; set; }
         public int GroupId { get; set; }
-      
+
         public DateTime SignTime { get; set; }
 
-      
+  
         public virtual Form IdOfFormNavigation { get; set; }
     }
 }
