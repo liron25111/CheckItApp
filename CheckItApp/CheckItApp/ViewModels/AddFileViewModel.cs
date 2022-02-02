9d9a95b943219edebef66cd948d44b2db928a178
User <User@LAB3-10>
2022-02-02 10:40:03 +0200

    push Form

---
+++ CheckItApp/CheckItApp/ViewModels/AddFileViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using CheckItApp.Views;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using CheckItApp.Services;
+using CheckItApp.Models;
+
+namespace CheckItApp.ViewModels
+{
+    class AddFileViewModel : BaseViewModel
+    {
+        private string error;
+        public string Error// get eror
+        {
+            get
+            {
+                return error;
+            }
+            set
+            {
+                if (error != value)
+                {
+                    error = value;
+                    OnPropertyChanged("Error");
+                }
+            }
+        }
+        public ICommand AddFileCommand { get; set; }// login Command
+        public void AddFile()
+        {
+         
+                        
+        }
+        public AddFileViewModel()
+        {
+            AddFileCommand = new Command(AddFile);
+
+        }
+    }
+}
